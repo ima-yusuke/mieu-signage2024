3fef6e73f5d9706a9548b8994b169fda444feb8b
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-12-03 18:44:14 +0900

    add：ローディング追加

---
+++ resources/js/loading.js
@@ -0,0 +1,40 @@
+const LoadingContainer = document.getElementById('loading_container');
+const CategoryContainer = document.getElementById('category_container');
+
+async function waitForVideoLoad(video) {
+    return new Promise((resolve) => {
+        if (video.readyState >= 4) {
+            resolve();
+        } else {
+            video.addEventListener('canplaythrough', () => {
+                resolve();
+            });
+        }
+    });
+}
+
+async function init() {
+    const droneVideo = document.getElementById('myVideo');
+    const videos = document.getElementsByClassName('video');
+
+    // 動画のロード完了を待機
+    await waitForVideoLoad(droneVideo);
+
+    for (let i = 0; i < videos.length; i++) {
+        await waitForVideoLoad(videos[i]);
+    }
+
+    // ローディング画面を非表示
+    LoadingContainer.style.display = 'none';
+    // CategoryContainer.classList.remove('hidden');
+
+    // 次のフレームでアニメーションを開始
+    requestAnimationFrame(() => {
+        CategoryContainer.classList.add('fade-in', 'show');
+    });
+}
+
+init();
+
+
+
